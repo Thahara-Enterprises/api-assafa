7ee5395dd92ca608c5b77928237203234423f661
sheikh-zafar <audios.shkzafar@gmail.com>
2023-08-14 15:22:15 +0400

    commit

--- pages/api/meal.js
+++ pages/api/meal.js
@@ -1,6 +1,18 @@
 import meal from '/data.json';
+import fs from 'fs';
+import path from 'path';
+const fsp = require('fs').promises;
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(meal);
-  } }
+export default async function handler(req, res) {
+  try {
+    const filePath = path.join(process.cwd(), `/data.json`);
+
+    // Read the JSON file
+    const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    // Do stuff
+    res.status(200).json(jsonData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Error reading data' });
+  }
+}
